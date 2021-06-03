6f831dc0d182315070c457e4fe1c60e268fac0fa
Dhruthi S <dhruthigowda08@gmail.com>
2021-06-03 15:54:58 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FindProductsDemo.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="Label1" runat="server" Text="Find Product"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+
+        <asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+        <br />
+        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br />
+        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox><br />
+        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox><br />
+        <asp:Button ID="btn_find" runat="server" OnClick="btn_find_Click" Text="Button" />
+        <hr />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <asp:GridView ID="GridView1" runat="server" AllowPaging="True">
+        </asp:GridView>
+        <hr />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BAL;
+using DAL;
+
+namespace FindProductsDemo
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_find_Click(object sender, EventArgs e)
+        {
+            string id = TextBox1.Text;
+            Products pb = new Products();
+            ProductsDAL pd = new ProductsDAL(); 
+            pb = pd.FindProduct(Convert.ToInt32(id));
+            TextBox2.Text = pb.ProductName;
+            TextBox3.Text = pb.QtyPerUnit;
+            TextBox4.Text = pb.SupplierID.ToString();
+            TextBox5.Text = pb.CategoryID.ToString();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            List<Products> fl = new List<Products>();
+            Products pb = new Products();
+            ProductsDAL pd = new ProductsDAL();
+            fl = pd.GetProducts();
+            GridView1.DataSource = fl;
+            GridView1.DataBind();
+        }
+    }
+}
